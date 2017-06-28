c933c5ca2e29d562ea6f35e4a4748adedb8b507e
ArthurSvezhentsev <celt244@gmail.com>
2017-06-28 11:50:54 +0300

    adress book

---
+++ address_book/address_book/src/com/levelup/Main.java
@@ -0,0 +1,17 @@
+package com.levelup;
+
+import com.levelup.view.MyDataTableFrame;
+import com.levelup.view.impl.CitizenTablePanel;
+
+import javax.swing.*;
+
+/**
+ * Created by java on 10.01.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new MyDataTableFrame();
+    }
+
+}
