5e4aeeb99ea9a66f7f35a58dd70fd80bcee8bd9f
ArthurSvezhentsev <celt244@gmail.com>
2017-07-06 00:36:54 +0300

    spring test

--- spring-test/src/main/java/ua/dp/levelup/web/config/AppConfig.java
+++ spring-test/src/main/java/ua/dp/levelup/web/config/AppConfig.java
@@ -1,6 +1,7 @@
 package ua.dp.levelup.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,24 +13,9 @@ import org.springframework.web.servlet.view.JstlView;
 /**
  * Created by java on 04.07.2017.
  */
-@EnableWebMvc
 @Configuration
 @ImportResource("WEB-INF/applicationContext.xml")
+@ComponentScan(basePackages = "ua.dp.levelup")
 public class AppConfig  extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
-
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver =
-                new InternalResourceViewResolver();
-//        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/pages/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
 }
